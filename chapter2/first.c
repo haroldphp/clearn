829638f5cc2e2ea6d12fbef1c65d69b1d327dc71
huanglong <long.huang@weicheche.cn>
2017-07-14 12:56:45 +0800

    first.c

---
+++ chapter2/first.c
@@ -0,0 +1,17 @@
+//
+// Created by huanglong on 2017/7/14.
+//
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    num = 1;
+
+    printf("I am a simple");
+    printf("computer.\n");
+    printf("My favorite number is %d because it is first.\n",num);
+
+    return 0;
+
+}
